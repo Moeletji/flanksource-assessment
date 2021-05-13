5674a294b6a1433fc3fdb3d0fbf580a782ea479f
Moeletji <mdsemenya@gmail.com>
2021-05-13 12:26:23 +0200

    Display teams on table

---
+++ src/components/Table.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+
+
+const Table = ({ fixtures}) => {
+
+    const [uniqueTeams, setUniqueTeams] = useState([]);
+
+    useEffect(() => {
+        getTeams()
+    }, []);
+
+    const getTeams = () => {
+        let teams = [];
+        fixtures.forEach(fixture => {
+            teams = [...teams, ...Object.keys(fixture.score)];
+        });
+        setUniqueTeams([...new Set(teams)]);
+    };
+
+    return (
+        <div style={{marginLeft:"auto", marginRight:"auto"}}>
+            <h1 id='title'>React Dynamic Table</h1>
+            <table id='students'>
+                <thead>
+                    <tr>
+                        <th>Team</th>
+                        <th>Games Played</th>
+                        <th>Win</th>
+                        <th>Loss</th>
+                        <th>Draw</th>
+                        <th>Goal difference</th>
+                        <th>Points</th>
+                    </tr>
+                </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        {
+                            uniqueTeams.map(team => <tr key={team}>
+                                {team}
+                            </tr>)
+                        }
+                    </td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Table;
